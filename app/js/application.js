d155399e2f32455de5a7f8dfbac844b2fbda161c
Jose Santana <josesantana.design@gmail.com>
2016-07-06 20:31:31 -0400

    Angular

--- app/js/application.js
+++ app/js/application.js
@@ -1,3 +1,15 @@
 /**================================================
-JS : MY CUSTOM SCRIPTS
+Controller = mainCtrl
 ===================================================*/
+'use strict';
+
+
+angular.module('venemoviesApp', [])
+	.controller('mainCtrl', ['$scope',  '$http', function($scope, $http) {
+	// Consumiendo archivo JSON
+		$http.get('js/data.json').success(function(data){
+			$scope.movies = data;
+		});
+}]);
+
+/*------------- Fin de Controller = mainCtrl--------------*/
